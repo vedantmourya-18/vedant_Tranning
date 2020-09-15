541fb98db5dd849a46ab677dfbc25c2bc2d6d9dc
Vedant Mourya <vedantmourya18@gmail.com>
2020-09-15 08:04:09 +0530

    session

---
+++ session_1/android/app/src/main/kotlin/com/vedant/session_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vedant.session_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ session_1/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title:Text('session_1'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Text('vedant'),
+      )
+    )
+  ));
+}
